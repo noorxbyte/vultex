1f2a0902ec079feefa84059e03863eb03d087a53
noorxbyte <noor.xbyte@gmail.com>
2015-11-10 17:59:06 +0500

    seed users table

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,8 +15,11 @@ class UsersTableSeeder extends Seeder
         DB::table('users')->delete();
  
         $users = array(
-            ['id' => 1, 'name' => 'Demo User', 'email' => 'user@demo.com', 'password' => bcrypt('123456'), 'created_at' => new DateTime, 'updated_at' => new DateTime],
-            ['id' => 2, 'name' => 'Hussain Noor Mohamed', 'email' => 'noor.xbyte@gmail.com', 'password' => bcrypt('123456'), 'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['id' => 1, 'name' => 'Hussain Noor Mohamed', 'email' => 'noor.xbyte@gmail.com', 'password' => bcrypt('123456'), 'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['id' => 2, 'name' => 'Mohamed Arham', 'email' => 'c4.arham@gmail.com', 'password' => bcrypt('123456'), 'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['id' => 3, 'name' => 'Mohamed Aman Shareef', 'email' => 'mohamed_aman@outlook.com', 'password' => bcrypt('123456'), 'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['id' => 4, 'name' => 'Mohamed Zihan Zahir', 'email' => 'xihan_xahir@hotmail.com', 'password' => bcrypt('123456'), 'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['id' => 5, 'name' => 'Abdulla Yaniu Jaleel', 'email' => 'jalylyaaniu@gmail.com', 'password' => bcrypt('123456'), 'created_at' => new DateTime, 'updated_at' => new DateTime],
         );
  
         // Uncomment the below to run the seeder
