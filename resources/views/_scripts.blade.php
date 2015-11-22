94b074a45f50a51e48b9060a3e0d142b61528326
noorxbyte <noor.xbyte@gmail.com>
2015-11-22 23:44:33 +0500

    added share buttons

--- resources/views/_scripts.blade.php
+++ resources/views/_scripts.blade.php
@@ -7,3 +7,8 @@
   js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=788092467961510";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
+
+
+
+<!-- Share Buttons -->
+<script>var sharebutton_is_horizontal = false; document.write('<script src="//cdn.script.to/share6.js"></scr' + 'ipt>'); document.write("<div style='display: none'>");</script><a href="http://sharebutton.org/">add share buttons</a><script>document.write("</div>");</script>
